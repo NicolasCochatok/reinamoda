7ddfa329cd7e6c23c306349ad0205d2e54a152ca
Nicolas Gauna <nicolas.cochatok@gmail.com>
2025-05-11 18:26:08 -0300

    Error en Accout

--- accounts/views.py
+++ accounts/views.py
@@ -15,7 +15,6 @@ from carts.models import Cart, CartItem
 import requests
 
 
-# Create your views here.
 def register(request):
     form = RegistrationForm()
     if request.method == 'POST':
@@ -26,34 +25,29 @@ def register(request):
             phone_number = form.cleaned_data['phone_number']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-            username = email.split("@")[0]  # FALTA PHONE NUMBEEEEEEEEEEEEEEEER
+            username = email.split("@")[0]
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
 
             profile = UserProfile()
-            profile.user_id = user.id
+            profile.user = user
             profile.profile_picture = 'default/default-user.png'
             profile.save()
-
 
             current_site = get_current_site(request)
             mail_subject = 'Activa tu cuenta en ByteShop para continuar'
             body = render_to_string('accounts/account_verification_email.html', {
                 'user': user,
                 'domain': current_site,
-                'uid': urlsafe_base64_encode( force_bytes(user.pk)),
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
                 'token': default_token_generator.make_token(user),
             })
             to_email = email
             send_email = EmailMessage(mail_subject, body, to=[to_email])
             send_email.send()
 
-
-            # messages.success(request, 'Te has registrado exitosamente')
-            return redirect('/accounts/login/?command=verification&email='+email)
-
-
+            return redirect('/accounts/login/?command=verification&email=' + email)
 
     context = {
         'form': form
@@ -74,11 +68,10 @@ def login(request):
                 is_cart_item_exist = CartItem.objects.filter(cart=cart).exists()
                 if is_cart_item_exist:
                     cart_item = CartItem.objects.filter(cart=cart)
-
                     product_variation = []
 
                     for item in cart_item:
-                        variation = item.variation.all()  #variations en realidad
+                        variation = item.variation.all()
                         product_variation.append(list(variation))
 
                     cart_item = CartItem.objects.filter(user=user)
@@ -95,7 +88,7 @@ def login(request):
                             index = ex_var_list.index(pr)
                             item_id = id[index]
                             item = CartItem.objects.get(id=item_id)
-                            item.quantity +=1
+                            item.quantity += 1
                             item.user = user
                             item.save()
                         else:
@@ -106,7 +99,6 @@ def login(request):
             except:
                 pass
 
-
             auth.login(request, user)
             messages.success(request, 'Has iniciado sesión exitosamente')
 
@@ -115,8 +107,7 @@ def login(request):
                 query = requests.utils.urlparse(url).query
                 params = dict(x.split('=') for x in query.split('&'))
                 if 'next' in params:
-                    nextPage = params['next']
-                    return redirect(nextPage)
+                    return redirect(params['next'])
             except:
                 return redirect('dashboard')
 
@@ -131,7 +122,6 @@ def login(request):
 def logout(request):
     auth.logout(request)
     messages.success(request, 'Has salido de sesión')
-
     return redirect('login')
 
 
@@ -139,7 +129,7 @@ def activate(request, uidb64, token):
     try:
         uid = urlsafe_base64_decode(uidb64).decode()
         user = Account._default_manager.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+    except (TypeError, ValueError, OverflowError, Account.DoesNotExist):
         user = None
 
     if user is not None and default_token_generator.check_token(user, token):
@@ -154,18 +144,18 @@ def activate(request, uidb64, token):
 
 @login_required(login_url='login')
 def dashboard(request):
-
     orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
     orders_count = orders.count()
 
-    userprofile = UserProfile.objects.get(user_id=request.user.id)
+    try:
+        userprofile = UserProfile.objects.get(user_id=request.user.id)
+    except UserProfile.DoesNotExist:
+        userprofile = UserProfile.objects.create(user=request.user)
 
     context = {
         'orders_count': orders_count,
         'userprofile': userprofile,
     }
-
-
     return render(request, 'accounts/dashboard.html', context)
 
 
@@ -200,8 +190,8 @@ def resetpassword_validate(request, uidb64, token):
     try:
         uid = urlsafe_base64_decode(uidb64).decode()
         user = Account._default_manager.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
-        user=None
+    except (TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
 
     if user is not None and default_token_generator.check_token(user, token):
         request.session['uid'] = uid
@@ -227,16 +217,16 @@ def resetPassword(request):
         else:
             messages.error(request, 'La contraseña de confirmación no concuerda')
             return redirect('resetPassword')
-    else:
-        return render(request, 'accounts/resetPassword.html')
+
+    return render(request, 'accounts/resetPassword.html')
 
 
+@login_required(login_url='login')
 def my_orders(request):
     orders = Order.objects.filter(user=request.user, is_ordered=True).order_by('-created_at')
     context = {
         'orders': orders,
     }
-
     return render(request, 'accounts/my_orders.html', context)
 
 
@@ -249,7 +239,7 @@ def edit_profile(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.success(request, 'Su información fue guardada con exito')
+            messages.success(request, 'Su información fue guardada con éxito')
             return redirect('edit_profile')
     else:
         user_form = UserForm(instance=request.user)
@@ -260,7 +250,6 @@ def edit_profile(request):
         'profile_form': profile_form,
         'userprofile': userprofile,
     }
-
     return render(request, 'accounts/edit_profile.html', context)
 
 
@@ -278,7 +267,6 @@ def change_password(request):
             if success:
                 user.set_password(new_password)
                 user.save()
-
                 messages.success(request, 'La contraseña se actualizó correctamente')
                 return redirect('change_password')
             else:
