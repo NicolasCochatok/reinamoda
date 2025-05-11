f615f70afec3a1d52a73b9b3285ba94cf8794aa2
Nicolas Gauna <nicolas.cochatok@gmail.com>
2025-05-11 12:41:43 -0300

    Versión rota para debug

--- store/models.py
+++ store/models.py
@@ -4,20 +4,17 @@ from django.urls import reverse
 from accounts.models import Account
 from django.db.models import Avg, Count
 
-
-# Create your models here.
 class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
-    slug = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField(max_length=500, blank=True)
     price = models.IntegerField()
-    images = models.ImageField(upload_to='photos/products')
+    image = models.ImageField(upload_to='photos/products')  # Cambiado a singular
     stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
-
 
     def get_url(self):
         return reverse('product_detail', args=[self.category.slug, self.slug])
@@ -32,7 +29,6 @@ class Product(models.Model):
             avg = float(reviews['average'])
         return avg
 
-
     def countReview(self):
         reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(count=Count('id'))
         count = 0
@@ -40,14 +36,12 @@ class Product(models.Model):
             count = int(reviews['count'])
         return count
 
-
 class VariationManager(models.Manager):
     def colors(self):
-        return super(VariationManager, self).filter(variation_category='color', is_active=True)
+        return super().filter(variation_category='color', is_active=True)
 
     def tallas(self):
-        return super(VariationManager, self).filter(variation_category='talla', is_active=True)
-
+        return super().filter(variation_category='talla', is_active=True)
 
 variation_category_choice = (
     ('color', 'color'),
@@ -63,10 +57,8 @@ class Variation(models.Model):
 
     objects = VariationManager()
 
-
     def __str__(self):
-        return self.variation_category + ' : ' + self.variation_value
-
+        return f"{self.variation_category}: {self.variation_value}"
 
 class ReviewRating(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -81,7 +73,6 @@ class ReviewRating(models.Model):
 
     def __str__(self):
         return self.subject
-
 
 class ProductGallery(models.Model):
     product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
